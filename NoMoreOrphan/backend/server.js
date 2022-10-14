de8e741c5b1a31011e1dde5694b3cc12ce0fa19d
nilesh <nilgupta2002@gmail.com>
2022-10-14 18:43:59 +0530

    second

--- NoMoreOrphan/backend/server.js
+++ NoMoreOrphan/backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const OrphanModel = require("./orphanModel");
 const DoctorModel = require("./doctorModel");
+const MedicalDetailModel = require("./medicalDetailModel");
 const bcrypt = require("bcrypt");
 const app = express();
 app.use(cors());
@@ -25,18 +26,12 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+//ORPHAN API/////////////////////////////////////////////////////////////////////////////////////////////////
+
 app.get("/getOrphan", async (req, res) => {
   const orphan = await OrphanModel.find();
   if (orphan) {
     return res.send(orphan);
-  }
-  return res.send("No existing user");
-});
-
-app.get("/getDoctor", async (req, res) => {
-  const doctor = await DoctorModel.find();
-  if (doctor) {
-    return res.send(doctor);
   }
   return res.send("No existing user");
 });
@@ -59,37 +54,6 @@ app.post("/addOrphan", async (req, res) => {
     .then(() => {
       res.send({
         message: "Orphan with name " + name + " is successfully added",
-      });
-    })
-    .catch((err) => {
-      console.log("error occured ", err);
-    });
-});
-
-app.post("/addDoctor", async (req, res) => {
-  const name = req.body.name;
-  const expertize = req.body.expertize;
-  const age = req.body.age;
-  const email = req.body.email;
-  const experience = req.body.experience;
-  let date = new Date();
-  let temp = date.getTime() + email + name + experience + expertize + age;
-  let password = await bcrypt.hash(temp, 10);
-  const registerDoctor = new DoctorModel({
-    name: name,
-    expertize: expertize,
-    age: age,
-    experience: experience,
-    email: email,
-    password: password,
-  });
-  registerDoctor
-    .save()
-    .then(() => {
-      res.send({
-        message: "Doctor with name " + name + " is successfully added",
-        email: email,
-        password: password,
       });
     })
     .catch((err) => {
@@ -124,13 +88,69 @@ app.put("/updateOrphan/:id", async (req, res) => {
     });
 });
 
+app.delete("/deleteOrphan/:id", async (req, res) => {
+  const id = req.params.id;
+  OrphanModel.findByIdAndDelete(id)
+    .then((data) => {
+      res.send({
+        message: "Deleted successfully",
+      });
+    })
+    .catch((err) => {
+      console.log("some error occured " + err);
+      res.send({
+        message: "Failed to delete",
+      });
+    });
+});
+
+//DOCTOR API/////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.get("/getDoctor", async (req, res) => {
+  const doctor = await DoctorModel.find();
+  if (doctor) {
+    return res.send(doctor);
+  }
+  return res.send("No existing user");
+});
+
+app.post("/addDoctor", async (req, res) => {
+  const name = req.body.name;
+  const expertize = req.body.expertize;
+  const age = req.body.age;
+  const email = req.body.email;
+  const experience = req.body.experience;
+  const date = new Date();
+  let temp = date.getTime().toString();
+  const password = await bcrypt.hash(temp, 10);
+  const registerDoctor = new DoctorModel({
+    name: name,
+    expertize: expertize,
+    age: age,
+    experience: experience,
+    email: email,
+    password: password,
+  });
+  registerDoctor
+    .save()
+    .then(() => {
+      res.send({
+        message: "Doctor with name " + name + " is successfully added",
+        email: email,
+        password: temp,
+      });
+    })
+    .catch((err) => {
+      console.log("error occured ", err);
+    });
+});
+
 app.put("/updateDoctor/:id", async (req, res) => {
   const id = req.params.id;
   const name = req.body.name;
   const expertize = req.body.expertize;
   const age = req.body.age;
   const experience = req.body.experience;
-  console.log(name, experience, expertize, age);
   DoctorModel.findByIdAndUpdate(id, {
     name: name,
     expertize: expertize,
@@ -150,22 +170,6 @@ app.put("/updateDoctor/:id", async (req, res) => {
     });
 });
 
-app.delete("/deleteOrphan/:id", async (req, res) => {
-  const id = req.params.id;
-  OrphanModel.findByIdAndDelete(id)
-    .then((data) => {
-      res.send({
-        message: "Deleted successfully",
-      });
-    })
-    .catch((err) => {
-      console.log("some error occured " + err);
-      res.send({
-        message: "Failed to delete",
-      });
-    });
-});
-
 app.delete("/deleteDoctor/:id", async (req, res) => {
   const id = req.params.id;
   DoctorModel.findByIdAndDelete(id)
@@ -179,6 +183,104 @@ app.delete("/deleteDoctor/:id", async (req, res) => {
       res.send({
         message: "Failed to delete",
       });
+    });
+});
+
+// DOCTOR LOGIN AND CHANGE PASSWORD API//////////////////////////////////////////////////////////////////////
+
+app.put("/changePasswordDoctor", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const newpass = req.body.newpass;
+
+  const doctor = await DoctorModel.findOne({ email: email });
+  if (doctor) {
+    if (await bcrypt.compare(password, doctor.password)) {
+      const temp = await bcrypt.hash(newpass, 10);
+      DoctorModel.findOneAndUpdate(
+        { email: email },
+        {
+          password: temp,
+        }
+      )
+        .then((result) => {
+          console.log(result);
+          res.send({
+            message: "password changed successfully",
+          });
+        })
+        .catch((err) => {
+          res.send({
+            message: "failed to update" + err,
+          });
+        });
+    }
+  } else {
+    res.send({
+      message: "No such user found",
+    });
+  }
+});
+
+app.get("/loginDoctor", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const doctor = await DoctorModel.findOne({ email: email });
+  if (doctor) {
+    if (await bcrypt.compare(password, doctor.password)) {
+      return res.send({
+        message: doctor,
+      });
+    }
+  } else {
+    res.send({
+      message: "No such user found",
+    });
+  }
+});
+
+// Add Medical Details API//////////////////////////////////////////////////////////////////////
+
+app.get("/getMedicalDetail", async (req, res) => {
+  try {
+    const medicalDetail = await MedicalDetailModel.find();
+    if (medicalDetail) {
+      res.send({
+        message: medicalDetail,
+      });
+    } else {
+      res.send("No medical details found");
+    }
+  } catch (error) {
+    console.log("some error occured" + error);
+  }
+});
+
+app.post("/addMedicalDetail/:id", async (req, res) => {
+  const id = req.params.id;
+  const bloodGroup = req.body.bloodGroup;
+  const allergies = req.body.allergies;
+  const symptoms = req.body.symptoms;
+  const diagonsis = req.body.diagonsis;
+  const treatment = req.body.treatment;
+  const registerMedicalDetail = new MedicalDetailModel({
+    orphanId: id,
+    bloodGroup: bloodGroup,
+    allergies: allergies,
+    symptoms: symptoms,
+    diagonsis: diagonsis,
+    treatment: treatment,
+  });
+  registerMedicalDetail
+    .save()
+    .then(() => {
+      res.send({
+        message: "Medical details is successfully added",
+      });
+    })
+    .catch((err) => {
+      console.log("error occured ", err);
     });
 });
 
